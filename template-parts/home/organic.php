d5a2bfae7582e51701e61476599905bbde2ff50e
jenea130 <jenea130@mail.ru>
2024-05-08 20:10:32 +0300

    feat: organic

--- template-parts/home/organic.php
+++ template-parts/home/organic.php
@@ -18,44 +18,6 @@ $product_posts = new WP_Query([
       </div>
       <?php btnComponent('#', $button_text, 'btn--accent'); ?>
     </div>
-
-    <div class="products__wrap">
-      <?php if ($product_posts->have_posts()) : ?>
-        <?php while ($product_posts->have_posts()) : ?>
-          <?php $product_posts->the_post(); ?>
-          <?php
-          $image = get_the_post_thumbnail_url();
-          $title = get_the_title();
-          $permalink = get_the_permalink();
-          $products_info = get_field('products_info');
-          $previous_price = $products_info['previous_price'];
-          $current_price = $products_info['current_price'];
-          $rating = $products_info['rating'];
-          $terms = get_the_terms(get_the_ID(), 'product-category');
-          $cat = $terms[0]->name;
-          ?>
-
-          <a href="<?php echo $permalink; ?>" class="products__item">
-            <div class="products__img">
-              <img src="<?php echo $image; ?>" alt="">
-            </div>
-            <div class="products__body">
-              <div class="products__cat">
-                <span><?php echo $cat; ?></span>
-              </div>
-              <h4 class="products__subtitle"><?php echo $title; ?></h4>
-              <div class="products__footer">
-                <span class="products__previous-price"><?php echo $previous_price; ?></span>
-                <span class="products__current-price"><?php echo $current_price; ?></span>
-                <div class="products__rating">
-                  <?php ratingComponent($rating); ?>
-                </div>
-              </div>
-            </div>
-          </a>
-        <?php endwhile; ?>
-        <?php wp_reset_postdata(); ?>
-      <?php endif; ?>
-    </div>
+    <?php productsLoopComponent($product_posts->posts); ?>
   </div>
 </div>
