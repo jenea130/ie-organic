4fd2cee21d34006f4598b36d323517ded1aac497
jenea130 <jenea130@mail.ru>
2024-05-23 16:17:31 +0300

    upd: news-info

--- template-parts/single-news/news-info.php
+++ template-parts/single-news/news-info.php
@@ -1,16 +1,22 @@
+<?php 
+$for_loop = get_field('for_loop');
+$text = $for_loop['text'];
+$title = get_the_title();
+$date = get_the_date();
+?>
 <div class="news-info">
   <header class="news-info__header">
     <div class="news-info__date">
-      <span><strong>Posted On:</strong> January 6, 2022</span>
+
+      <span><strong>Posted On: </strong><?php echo $date; ?></span>
     </div>
     <div class="news-info__author">
-
       <div class="news-info__logo">
         <?php get_template_part('template-parts/icons/icon-man'); ?>
       </div>
       <span>By Rachi Card</span>
     </div>
   </header>
-  <h1 class="news-info__title title-intro">Research More Organic Food</h1>
-  <div class="news-info__text text">Established fact that a reader will be distracted by the readable content of a page when looking a layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed</div>
+  <h1 class="news-info__title title-intro"><?php echo $title ?></h1>
+  <div class="news-info__text text"><?php echo $text ?></div>
 </div>
